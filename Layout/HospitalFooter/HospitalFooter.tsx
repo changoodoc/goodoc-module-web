08be396ac13d815774654c61d55c138f808f321c
kei.ko <kei.ko@goodoc.co.kr>
2019-08-23 13:42:43 +0900

    fix: 병원어드민 푸터 staging 내용 반영

--- Layout/HospitalFooter/HospitalFooter.tsx
+++ Layout/HospitalFooter/HospitalFooter.tsx
@@ -1,67 +1,86 @@
 import * as React from "react";
-import HospitalFooterModel from './HospitalFooterModel';
+import { HospitalFooterLogo, HospitalFooterModel } from "./HospitalFooterModel";
 
-const HospitalFooter = (DividerVertical: any, FooterUrls: any, FooterMenualFiles: any) => {
+const HospitalFooter = (VersionTooltip: any, DividerVertical: any, FooterMenualFiles: any) => {
     return ({
-        isLogin = true,
-        isFix = false
-      }: {
+                isLogin = true,
+                isFix = false
+            }: {
         isLogin?: boolean;
         isFix?: boolean;
-      }) => {
-    return (
-        <footer className={`Footer${isFix ? " fix" : ""}`}>
-            <div className="Footer__container">
-                <section className="Footer__section1">
-                <div className="Footer__col1">
-                    <img className="Footer__logo" src={logo} alt="Carelabs Logo" />
-                    <div className="Footer__col1--section1">
-                        {HospitalFooterModel.phone}
-                    <br />
-                    {HospitalFooterModel.email}
-                    <br />
-                    <span>
-                    {HospitalFooterModel.allRight}{" "}
-                        <span className="Footer__subText">v0.1.0</span>
-                    </span>
-                    </div>
-                    <div className="Footer__col1--section2">
-                    {[
-                        HospitalFooterModel.componayName,
-                        HospitalFooterModel.regCode,
-                        HospitalFooterModel.ceo,
-                        HospitalFooterModel.addr,
-                        HospitalFooterModel.goodocCEO,
-                        HospitalFooterModel.code
-                    ].map((item, idx, arr) => {
-                        return (
-                            <>
-                                {item}
-                                {
-                                    idx % 3 ?
-                                      <DividerVertical color="black" /> :
-                                      (idx === 3 ? <br /> : '')
-                                }
-                            </>
-                        )
-                    })}
-                    </div>
-                    <div className="Footer__col1--section3">
-                    {isLogin ? <FooterUrls /> : null}
-                    </div>
+    }) => {
+        const openNewLink = (target: "usage" | "privacy" | "alliance") => () => {
+            const host = 'https://hello-staging.goodoc.co.kr';
+            switch (target) {
+                case "alliance":
+                    window.open(`${host}/allance`);
+                    break;
+                default:
+                    window.open(`${host}/term#${target}`);
+                    break;
+            }
+        };
+        return (
+            <footer className={`Footer${isFix ? " fix" : ""}`}>
+                <div className="Footer__container">
+                    <section className="Footer__section1">
+                        <div className="Footer__col1">
+                            <div className="Footer__col1--section1">
+                                <div
+                                    className="Footer__col1--section1 usage"
+                                    onClick={openNewLink("usage")}
+                                >
+                                    {HospitalFooterModel.usage}
+                                </div>
+                                <div
+                                    className="Footer__col1--section1 privacy"
+                                    onClick={openNewLink("privacy")}
+                                >
+                                    {HospitalFooterModel.privacy}
+                                </div>
+                                <div
+                                    className="Footer__col1--section1 alliance"
+                                    onClick={openNewLink("alliance")}
+                                >
+                                    {HospitalFooterModel.alliance}
+                                </div>
+                            </div>
+                            <div className="Footer__col1--section2">
+                                <img className="Footer__logo" src={HospitalFooterLogo} alt="Carelabs Logo" />
+                                <div className="Footer__info">
+                                    {
+                                        [
+                                            HospitalFooterModel.componayName,
+                                            HospitalFooterModel.ceo,
+                                            HospitalFooterModel.regCode,
+                                            HospitalFooterModel.phone,
+                                            HospitalFooterModel.code,
+                                            HospitalFooterModel.addr,
+                                            HospitalFooterModel.allRight
+                                        ].map((item, idx) => {
+                                            return (
+                                                <>
+                                                    {item}
+                                                    {idx === 4 || idx === 6 ?
+                                                        (idx === 4 ? <br/> : <VersionTooltip/>)
+                                                        :
+                                                        <DividerVertical color={idx === 5 ? 'black' : 'gray'} />
+                                                    }
+                                                </>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`Footer__col2${isLogin ? "" : " bottom"}`}>
+                            <div className="Footer__col2--section1">
+                                {isLogin ? <FooterMenualFiles /> : null}
+                            </div>
+                        </div>
+                    </section>
                 </div>
-                <div className={`Footer__col2${isLogin ? "" : " bottom"}`}>
-                    <div className="Footer__col2--section1">
-                    {isLogin ? <FooterMenualFiles /> : <FooterUrls />}
-                    </div>
-                </div>
-                </section>
-                <div className="Footer__divider" />
-                <section className="Footer__section2">
-                {HospitalFooterModel.desc}
-                </section>
-            </div>
-        </footer>
+            </footer>
         );
     };
 };
