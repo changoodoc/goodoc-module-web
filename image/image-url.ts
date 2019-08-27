aa1c4c2c0e993014278073b3179b80880774edea
Austin <austin.park@goodoc.co.kr>
2019-08-27 14:47:16 +0900

    refactor: origin 이미지 경로 수정

--- image/image-url.ts
+++ image/image-url.ts
@@ -4,7 +4,10 @@ class GDWebModuleImageUrl {
   }
   private _imageV2Url: string;
   constructor(private readonly imageUrl: string) {}
-  getImageUrlByV2(): string {
+  /**
+   * @param origin = "https://staging.goodoc.co.kr"
+   */
+  getImageUrlByV2(originPath: string = ''): string {
     if (!this._imageV2Url) {
       let result = this.imageUrl;
       // "/uploads/hospital_notice/image/10004/http%3A/asset-staging.goodoc.kr/images/files/20190827/bbaded0aae64120b81a7ee466cc81c44.jpeg"
@@ -12,6 +15,8 @@ class GDWebModuleImageUrl {
       if (index > -1) {
         // "http:/asset-staging.goodoc.kr/images/files/20190827/bbaded0aae64120b81a7ee466cc81c44.jpeg"
         result = decodeURIComponent(result.substring(index));
+      } else {
+        result = originPath + result;
       }
       this._imageV2Url = result;
     }
