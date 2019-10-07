3edb0507722ceddc7aa562314f4c16b4e8f9f4f9
Charles <charles.han@goodoc.co.kr>
2019-10-07 17:44:18 +0900

    feat: 해피톡 popup 모듈화
    WF-305

---
+++ Layout/HappyTalk/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+
+import './style.css';
+import Icon from './Icon';
+
+interface ButtonHappyTalk {
+  href: string;
+  style?: React.CSSProperties;
+  windowSize?: {
+    width: number;
+    height: number;
+  }
+}
+
+const ButtonHappyTalk: React.FC<ButtonHappyTalk> = ({ href, style }) => {
+  const innerWidth = useRef(0);
+
+  useEffect(() => {
+    innerWidth.current = window.innerWidth / 2;
+  }, []);
+
+  const openHappyTalk = () => {
+    window.open(
+      href,
+      "",
+      `width=500, height=800, left=${innerWidth.current - 125}`
+    );
+  }
+
+  return (
+    <button onClick={openHappyTalk} className="HappyTalk__button" style={style}>
+      <Icon />
+      <span className="HappyTalk__button--title">1:1 채팅상담</span>
+    </button>
+  )
+}
+
+export default ButtonHappyTalk;
