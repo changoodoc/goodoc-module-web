4617f43f3fc4d3a0f218aa56d40c5a2cd89b78dc
jeus.han <jeus.han@goodoc.co.kr>
2019-10-11 18:29:15 +0900

    feat: 푸터 영역 디자인 시안 기준 수정

--- Layout/HospitalFooter/HospitalFooter.tsx
+++ Layout/HospitalFooter/HospitalFooter.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { HOSPITAL_FOOTER_TXT, HOSPITAL_FOOTER_LOGO } from "./HospitalFooterModel";
 import "./HospitalFooter.scss";
 
-const hostname = window.location.hostname;
-const link = hostname === 'www.goodoc.co.kr' || hostname === 'goodoc.co.kr' ? 'https://hello.goodoc.co.kr' : 'https://hello-staging.goodoc.co.kr';
 const Empty = () => (<span />);
 const HospitalFooter = (DividerVertical: any, VersionTooltip: any = Empty, FooterMenualFiles: any = Empty) => {
     return ({
@@ -13,63 +11,23 @@ const HospitalFooter = (DividerVertical: any, VersionTooltip: any = Empty, Foote
         isLogin?: boolean;
         isFix?: boolean;
     }) => {
-        const openNewLink = (target: "usage" | "privacy" | "alliance") => () => {
-            switch (target) {
-                case "alliance":
-                    window.open(`${link}/alliance`);
-                    break;
-                default:
-                    window.open(`${link}/term/#/${target}`);
-                    break;
-            }
-        };
         return (
             <footer className={`Footer${isFix ? " fix" : ""}`}>
                 <div className="Footer__container">
                     <section className="Footer__section1">
                         <div className="Footer__col1">
-                            <div className="Footer__col1--section1">
-                                {/*<div*/}
-                                {/*    className="Footer__col1--section1 usage"*/}
-                                {/*    onClick={openNewLink("usage")}*/}
-                                {/*>*/}
-                                {/*    {HOSPITAL_FOOTER_TXT.usage}*/}
-                                {/*</div>*/}
-                                {/*<div*/}
-                                {/*    className="Footer__col1--section1 privacy"*/}
-                                {/*    onClick={openNewLink("privacy")}*/}
-                                {/*>*/}
-                                {/*    {HOSPITAL_FOOTER_TXT.privacy}*/}
-                                {/*</div>*/}
-                                <div
-                                    className="Footer__col1--section1 alliance"
-                                    onClick={openNewLink("alliance")}
-                                >
-                                    {HOSPITAL_FOOTER_TXT.alliance}
-                                </div>
-                            </div>
                             <div className="Footer__col1--section2">
                                 <img className="Footer__logo" src={HOSPITAL_FOOTER_LOGO} alt="Carelabs Logo" />
                                 <div className="Footer__info">
                                     {
                                         [
-                                            // HOSPITAL_FOOTER_TXT.componayName,
-                                            // HOSPITAL_FOOTER_TXT.ceo,
-                                            // HOSPITAL_FOOTER_TXT.regCode,
-                                            // HOSPITAL_FOOTER_TXT.phone,
-                                            // HOSPITAL_FOOTER_TXT.code,
                                             HOSPITAL_FOOTER_TXT.addr,
                                             HOSPITAL_FOOTER_TXT.allRight
                                         ].map((item, idx) => {
                                             return (
                                                 <span key={idx}>
-                                                    {item}
-                                                    {idx === 1 || idx === 1 ?
-                                                        (<VersionTooltip/> || '')
-                                                        :
-                                                        <DividerVertical color={idx === 5 ? 'black' : 'gray'} />
-                                                    }
-                                                </span>
+                                                {item}{idx === 1 || idx === 1 && (<VersionTooltip/> || '')}
+                                            </span>
                                             )
                                         })
                                     }
@@ -78,7 +36,9 @@ const HospitalFooter = (DividerVertical: any, VersionTooltip: any = Empty, Foote
                         </div>
                         <div className={`Footer__col2${isLogin ? "" : " bottom"}`}>
                             <div className="Footer__col2--section1">
-                                {isLogin ? (<FooterMenualFiles /> || '') : null}
+                                <FooterMenualFiles
+                                    isLogin={isLogin}
+                                />
                             </div>
                         </div>
                     </section>
