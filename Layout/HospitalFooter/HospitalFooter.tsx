fb06e5c693bd585a7725a8b657ed2c56141bdf39
kei.ko <kei.ko@goodoc.co.kr>
2020-06-24 10:23:19 +0900

    feat: 푸터 로고 alt 속성값 변경
    [굿닥 물적분할]
    
    ES-274

--- Layout/HospitalFooter/HospitalFooter.tsx
+++ Layout/HospitalFooter/HospitalFooter.tsx
@@ -17,7 +17,7 @@ const HospitalFooter = (DividerVertical: any, VersionTooltip: any = Empty, Foote
                     <section className="Footer__section1">
                         <div className="Footer__col1">
                             <div className="Footer__col1--section2">
-                                <img className="Footer__logo" src={HOSPITAL_FOOTER_LOGO} alt="Carelabs Logo" />
+                                <img className="Footer__logo" src={HOSPITAL_FOOTER_LOGO} alt="Goodoc Logo" />
                                 <div className="Footer__info">
                                     {
                                         [
